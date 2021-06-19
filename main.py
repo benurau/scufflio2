d5d373e8b34f12c97ae18964049b782e5509fe36
PythonBoost <emillaiho@hotmail.fi>
2021-06-19 15:08:02 +0300

    gloabl coords

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 #Import necessary libraries
 import matplotlib.pyplot as plt
 from flask import Flask, render_template, Response,jsonify,request
-
+from flask import g
 
 app = Flask(__name__)
 
-
 coordsx = []
 coordsy = []
+
 
 @app.route('/')
 def index():
@@ -19,37 +19,33 @@ def gues():
 
 @app.route('/gues_coords', methods = ['POST', 'GET'])
 def diez():
-  request.method = 'GET'
-  #request_data = request.json
-  d = {"currX": coordsx}
-  print(coordsx)
-  return jsonify(d)
-
+    request.method = 'GET'
+    #request_data = request.json
+    d = {"currX": coordsx}
+    return jsonify(d)
 
 
 @app.route('/scrib',methods = ['POST', 'GET'])
 def login():
+    global coordsx
+    global coordsy
 
+    if request.method == "POST":
+      request_data = request.json
 
-  #request.method = 'POST'
-  request_data = request.json
+      coordsx.append(request_data["currX"])
+      print(request.data)
+      coordsy.append(request_data["currY"])
 
-  if request.method == "POST":
-    coordsx.append(request_data["currX"])
-    print(request.data)
-    coordsy.append(request_data["currY"])
+      if len(coordsx) > 1000:
+        print(coordsx)
+        plt.scatter(x=coordsx, y=coordsy)
+        plt.show()
 
-    if len(coordsx) > 1000:
+    elif request.method == "GET":
+      d = {"currX": coordsx}
       print(coordsx)
-      plt.scatter(x=coordsx, y=coordsy)
-      plt.show()
-
-  if request.method == "GET":
-    request.method = 'GET'
-    d = {"currX": coordsx}
-    print(coordsx)
-    return jsonify(d)
-
+      return jsonify(d)
 
 
 
