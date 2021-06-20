bf529ac90150147a66119ffe3d76f3471e0c34b5
PythonBoost <emillaiho@hotmail.fi>
2021-06-20 12:55:50 +0300

    game,player classes

--- main.py
+++ main.py
@@ -8,6 +8,42 @@ app = Flask(__name__)
 coordsx = []
 coordsy = []
 
+class User:
+    def __init__(self, ip):
+        self.ip = ip
+        self.points = 0
+        self.n_painted = 0
+        self.guessed_correct = False
+
+    def add_points(self, points):
+        self.points += points
+
+    def add_painted(self):
+        self.n_painted += 1
+
+    def add_guessed_correct(self):
+        self.guessed_correct = True
+
+
+
+class Game():
+    def __init__(self):
+        self.round = 1
+        self.players = []
+
+    def add_player(self,user:User):
+        self.players.append(user)
+
+    def next_round(self):
+        self.round += 1
+
+    def get_current_round(self):
+        return self.round
+
+    def get_player_scores(self):
+        players = [(x,x.points) for x in self.players]
+        print(players)
+
 
 @app.route('/')
 def index():
@@ -19,11 +55,16 @@ def gues():
 
 @app.route('/get_words')
 def benu():
-    words = ["benu","pp","dummy","small"]
+    words = ["benu", "pp", "dummy", "small"]
     d = {"words": words}
+    print(d)
     return jsonify(d)
 
 
+@app.route("/get_my_ip", methods=["GET"])
+def get_my_ip():
+    print(request.remote_addr)
+    #return jsonify({'ip': request.remote_addr}), 200
 
 
 @app.route('/scrib',methods = ['POST', 'GET'])
